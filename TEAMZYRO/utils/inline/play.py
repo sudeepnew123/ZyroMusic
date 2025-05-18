0744caab35c91cefb24e981a3568ea5d1e147b0b
sudeepnew123 <sudeepmandal8999@gmail.com>
2025-05-18 07:47:19 +0530

    Update play.py

--- TEAMZYRO/utils/inline/play.py
+++ TEAMZYRO/utils/inline/play.py
@@ -80,12 +80,10 @@ def stream_markup(_, chat_id):
             InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
             InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
         ],
-
-        InlineKeyboardButton(text="Support", url="https://t.me/for4ever_friends")],
+        [InlineKeyboardButton(text="Support", url="https://t.me/for4ever_friends")],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
     ]
     return buttons
-
 
 def playlist_markup(_, videoid, user_id, ptype, channel, fplay):
     buttons = [
