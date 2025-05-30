0186bd4f1a56e4309d76144fac93ce58d040c593
sudeepnew123 <sudeepmandal8999@gmail.com>
2025-05-30 07:59:35 +0530

    Update play.py

--- TEAMZYRO/utils/inline/play.py
+++ TEAMZYRO/utils/inline/play.py
@@ -66,7 +66,7 @@ def stream_markup_timer(_, chat_id, played, dur):
             InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
         ],
 
-        [InlineKeyboardButton(text="𝐒𝐔𝐏𝐏𝐎𝐑𝐓 🎀", url="https://t.me/FOREVERFRANDS")],
+        [InlineKeyboardButton(text="sυρρᴏʀᴛ 𝖌яᴏυρ 🎀", url="https://t.me/FOREVERFRANDS")],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
     ]
     return buttons
