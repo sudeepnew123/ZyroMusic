28f490ad56f8b93b28b39f20beba1de563e6b9c4
sudeepnew123 <sudeepmandal8999@gmail.com>
2025-05-30 09:12:46 +0530

    Update queue.py

--- TEAMZYRO/utils/inline/queue.py
+++ TEAMZYRO/utils/inline/queue.py
@@ -67,10 +67,18 @@ def queue_back_markup(_, CPLAY):
 def aq_markup(_, chat_id):
     buttons = [
         [
-            InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
-            InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
-            InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
-            InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
+            InlineKeyboardButton(
+                text="⧈ ᴀᴅᴅ ᴍᴇ ɪɴ ʏᴏᴜʀ ɢʀᴏᴜᴘ ⧈",
+                url="https://t.me/Music_promaxbot?startgroup=true"
+            )
+        ],
+        [
+            InlineKeyboardButton(text="• ʀᴇsυᴍᴇ •", callback_data=f"ADMIN Resume|{chat_id}"),
+            InlineKeyboardButton(text="• ᴘᴀυsᴇ •", callback_data=f"ADMIN Pause|{chat_id}")
+        ],
+        [
+            InlineKeyboardButton(text="• sᴋɪᴘ •", callback_data=f"ADMIN Skip|{chat_id}"),
+            InlineKeyboardButton(text="• sᴛᴏᴘ •", callback_data=f"ADMIN Stop|{chat_id}")
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
     ]
